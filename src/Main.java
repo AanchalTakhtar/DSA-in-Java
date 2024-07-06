e1819d841688d32db231c9acb7c105bf13937394
Aanchaltakhtar-7 <118098589+Aanchaltakhtar-7@users.noreply.github.com>
2024-07-07 00:34:36 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main
+{
+    public static void main(String[] args) {
+
+        LinkedList mylinkedlist = new LinkedList(4);
+          //mylinkedlist.gethead();
+          //mylinkedlist.getlength();
+          //mylinkedlist.gettail();
+          mylinkedlist.append(8);
+          mylinkedlist.append(10);
+          mylinkedlist.append(12);
+          //mylinkedlist.prepend(2);
+          //mylinkedlist.set(2,11);
+          //mylinkedlist.insert(1,6);
+          //mylinkedlist.remove(3);
+          //mylinkedlist.reverse();
+
+          //System.out.println(mylinkedlist.get(8).value);
+
+         //System.out.println(mylinkedlist.removelast().value);
+         //System.out.println(mylinkedlist.removelast().value);
+         //System.out.println(mylinkedlist.removelast());
+
+         //System.out.println(mylinkedlist.removeFirst().value);
+         //System.out.println(mylinkedlist.removeFirst().value);
+         //System.out.println(mylinkedlist.removeFirst());
+
+        mylinkedlist.printlist();
+
+    }
+}
